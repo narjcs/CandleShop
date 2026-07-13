96e29b6df6f52c42852bc97a5a08ca3be8528019
Narjes <Sunarjem@gmail.com>
2026-07-13 14:53:17 +0330

    refactor(web): implement modular site layout and clean up boilerplate

---
+++ EShop.Web/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected string ErrorMessage = "ErrorMessage";
+        protected string SuccessMessage = "SuccessMessage";
+        protected string InfoMessage = "InfoMessage";
+        protected string WarningMessage = "WarningMessage";
+    }
+}

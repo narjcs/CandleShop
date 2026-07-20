81844cd570259f243fff8ad6ff27f25892f3e762
Narjes <Sunarjem@gmail.com>
2026-07-20 23:45:50 +0330

    feat(web): scaffold reusable ViewComponents for header, footer and cart

---
+++ EShop.Web/ViewComponents/HomeCartSectionViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Web.ViewComponents
+{
+    public class HomeCartSectionViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("HomeCartSection");
+        }
+    }
+}
---
+++ EShop.Web/ViewComponents/SiteHeaderViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Web.ViewComponents
+{
+    public class SiteHeaderViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("SiteHeader");
+        }
+    }
+}

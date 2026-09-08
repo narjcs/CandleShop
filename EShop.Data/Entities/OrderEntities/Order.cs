15a5f5e6dfef297cfa27257f969bc924939ddd4f
Narjes <Sunarjem@gmail.com>
2026-09-08 12:33:29 +0330

    feat(data): add Order and OrderDetail entities

---
+++ EShop.Data/Entities/OrderEntities/Order.cs
@@ -0,0 +1,8 @@
+﻿using EShop.Data.Entities.Common;
+
+namespace EShop.Data.Entities.OrderEntities
+{
+    public class Order : BaseEntity
+    {
+    }
+}
---
+++ EShop.Data/Entities/OrderEntities/OrderDetail.cs
@@ -0,0 +1,11 @@
+﻿using EShop.Data.Entities.Common;
+using EShop.Data.Entities.CandleEntities;
+
+namespace EShop.Data.Entities.OrderEntities
+{
+    public class OrderDetail : BaseEntity
+    {
+        public long CandleId { get; set; }
+        public Candle Candle { get; set; }
+    }
+}
